c984d4b251690ec9a5961a96a3caaa3b314ddd01
huy <huy@gmail.com>
2021-07-26 12:02:01 +0700

    assignment PHP Laravel

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/create',[\App\Http\Controllers\ApartmentController::class,'create']);
+Route::post('/store',[\App\Http\Controllers\ApartmentController::class,'store']);
+Route::get('/list',[\App\Http\Controllers\ApartmentController::class,'index']);
